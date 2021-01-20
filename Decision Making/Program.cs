8e02db59603b093dbfc17cdf538ed34da6833c8e
Nate <nsdavis@mtu.edu>
2021-01-20 15:37:36 -0500

    added formatting on the outputs

--- Decision Making/Program.cs
+++ Decision Making/Program.cs
@@ -12,7 +12,7 @@ namespace Decision_Making
             do
             {
 
-                Console.WriteLine("Please enter an integer between 1 and 100: ");
+                Console.WriteLine("\nPlease enter an integer between 1 and 100: ");
                 int input = int.Parse(Console.ReadLine());
 
                 if (input < 1 || input > 100)
@@ -20,31 +20,31 @@ namespace Decision_Making
                     Console.Beep();
                     Console.Beep();
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("HEY! That was not what I asked for!");
+                    Console.WriteLine("\nHEY! That was not what I asked for!!!");
                     Console.ResetColor();
                 }
                 else if (input % 2 == 0 && input < 25)
                 {
-                    Console.WriteLine($"{user} entered {input}, which is even and less than 25!");
+                    Console.WriteLine($"\n{user} entered {input}, which is even and less than 25!");
                 }
                 else if (input % 2 == 0 && input <= 60)
                 {
-                    Console.WriteLine($"{user} entered Even!");
+                    Console.WriteLine($"\n{user} entered Even!");
                 }
                 else if (input % 2 == 0 && input >= 60)
                 {
-                    Console.WriteLine($"{user} entered {input} and is Even!");
+                    Console.WriteLine($"\n{user} entered {input} and is Even!");
                 }
                 else
                 {
-                    Console.WriteLine($"{user} entered {input} and is odd!");
+                    Console.WriteLine($"\n{user} entered {input} and is odd!");
                 }
 
-                Console.WriteLine("Continue? (y/n)");
+                Console.WriteLine("\nContinue? (y/n)\n");
 
             } while (Console.ReadLine().ToLower() == "y");
 
-            Console.WriteLine($" Thank you for playing {user}, goodbye!");
+            Console.WriteLine($"\n Thank you for playing {user}, goodbye!\n");
 
            // Console.WriteLine("Continue? y/n");
         }
